d832dbb59605e310dcaada7a1234e8fd1725ba0b
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-07-05 21:08:08 -0500

    199 add settings customization for admins

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,14 +1,29 @@
 class SettingsController < ApplicationController
+  before_action :check_for_settings, only: [:edit]
+
   def update
-    @admin = Admin.find_by(id: params[:id])
-    @setting = Setting.new(
-      admin_id: @admin.id,
-      auto_text: params[:auto_text]
-    )
-    @setting.save
+    @setting = Setting.find_by(id: params[:id])
+    if @setting.update(setting_params)
+      flash[:success] = "Updated!"
+      redirect_to '/'
+    else
+      flash[:error] = "ERROR: We could not update this setting."
+      render :next
+    end
   end
 
   def edit
+    @setting = @admin.setting
+  end
+
+private
+
+  def check_for_settings
     @admin = Admin.find_by(id: params[:id])
+    @admin.setting.nil? ? @admin.create_setting() : @admin.setting
+  end
+  
+  def setting_params
+    params.require(:setting).permit(:auto_text)
   end
 end
