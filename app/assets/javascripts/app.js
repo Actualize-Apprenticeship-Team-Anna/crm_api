57715bfec9ac8cc42b9a0343f2378c2f9db2239b
Barry <bdc1130@yahoo.com>
2018-07-09 20:59:32 -0500

    creating the onclick to view events

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,11 +11,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
       $.get('/api/v1/leads.json').success(function(response) {
         console.log(this);
         this.leads = response;
+        for(var i = 0; i < this.leads.length; i++) {
+          this.leads[i].visible = false;
+        }
       }.bind(this));
     },
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      onClickLead: function(lead) {
+        lead.visible = true;
+        console.log(lead);
       }
     },
     computed: {
