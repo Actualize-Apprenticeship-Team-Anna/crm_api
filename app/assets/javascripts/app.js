1e4a3f033a88cb1863f3712f0a051d690d863baa
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-07-09 20:10:31 -0500

    216 - three dynamic search bars

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     data: {
       leads: [],
       time_format: "12/25/17",
-      url: "https://www.google.com/"
+      url: "https://www.google.com/", 
+      searchFirstName: "",
+      searchLastName: "",
+      searchEmail: ""
+
     },
     mounted: function() {
       $.get('/api/v1/leads.json').success(function(response) {
@@ -16,7 +20,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     methods: {
       moment: function(date) {
         return moment(date);
-      }
+      },
+    isValidLead: function(FirstName, LastName, Email){
+      console.log("hello");
+      var validFirstName = FirstName.toLowerCase().includes(this.searchFirstName.toLowerCase());
+
+      var validLastName = LastName.toLowerCase().includes(this.searchLastName.toLowerCase());
+
+      var validEmail = Email.toLowerCase().includes(this.searchEmail.toLowerCase());
+      return validFirstName && validLastName && validEmail;
+     }  
     },
     computed: {
 
